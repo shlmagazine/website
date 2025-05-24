e9f7437dcf32d3cdb42bb0a313ab3cd8d434f6c1
Yiu-On <14921674+yshyonli2@users.noreply.github.com>
2025-05-23 20:14:31 -0700

    Add portfolio, shop, and email to bio link options

--- plugins/shlm-functionality/shlm-functionality-profile-settings.php
+++ plugins/shlm-functionality/shlm-functionality-profile-settings.php
@@ -29,6 +29,18 @@ function add_social_media_fields($user) {
             </td>
         </tr>
         <tr>
+            <th><label for="portfolio-link">Portfolio</label></th>
+            <td>
+                <input type="text" name="portfolio_link" id="portfolio-link" value="<?php echo esc_attr( get_the_author_meta( 'portfolio_link', $user->ID ) ); ?>" class="regular-text" />
+            </td>
+        </tr>
+        <tr>
+            <th><label for="shop-link">Shop</label></th>
+            <td>
+                <input type="text" name="shop_link" id="shop-link" value="<?php echo esc_attr( get_the_author_meta( 'shop_link', $user->ID ) ); ?>" class="regular-text" />
+            </td>
+        </tr>
+        <tr>
             <th><label for="instagram-link">Instagram</label></th>
             <td>
                 <input type="text" name="instagram_link" id="instagram-link" value="<?php echo esc_attr( get_the_author_meta( 'instagram_link', $user->ID ) ); ?>" class="regular-text" />
@@ -64,13 +76,17 @@ function add_social_media_fields($user) {
                 <input type="text" name="facebook_link" id="facebook-link" value="<?php echo esc_attr( get_the_author_meta( 'facebook_link', $user->ID ) ); ?>" class="regular-text" />
             </td>
         </tr>
-        </tr>
         <tr>
             <th><label for="tumblr-link">Tumblr</label></th>
             <td>
                 <input type="text" name="tumblr_link" id="tumblr-link" value="<?php echo esc_attr( get_the_author_meta( 'tumblr_link', $user->ID ) ); ?>" class="regular-text" />
             </td>
         </tr>
+        <tr>
+            <th><label for="youtube-link">YouTube</label></th>
+            <td>
+                <input type="text" name="youtube_link" id="youtube-link" value="<?php echo esc_attr( get_the_author_meta( 'youtube_link', $user->ID ) ); ?>" class="regular-text" />
+            </td>
         </tr>
         <tr>
             <th><label for="email-link">Email</label></th>
@@ -91,6 +107,8 @@ function save_social_media_fields($user_id) {
     }
     
     update_user_meta( $user_id, 'linkedin_link', esc_url_raw($_POST['linkedin_link'] ));
+    update_user_meta( $user_id, 'portfolio_link', esc_url_raw($_POST['portfolio_link'] ));
+    update_user_meta( $user_id, 'shop_link', esc_url_raw($_POST['shop_link'] ));
     update_user_meta( $user_id, 'instagram_link', esc_url_raw($_POST['instagram_link'] ));
     update_user_meta( $user_id, 'x_link', esc_url_raw($_POST['x_link'] ));
     update_user_meta( $user_id, 'bluesky_link', esc_url_raw($_POST['bluesky_link'] ));
@@ -98,6 +116,7 @@ function save_social_media_fields($user_id) {
     update_user_meta( $user_id, 'tiktok_link', esc_url_raw($_POST['tiktok_link'] ));
     update_user_meta( $user_id, 'facebook_link', esc_url_raw($_POST['facebook_link'] ));
     update_user_meta( $user_id, 'tumblr_link', esc_url_raw($_POST['tumblr_link'] ));
+    update_user_meta( $user_id, 'youtube_link', esc_url_raw($_POST['youtube_link'] ));
     update_user_meta( $user_id, 'email_link', esc_url_raw($_POST['email_link'] ));
 }
 
@@ -121,7 +140,7 @@ function get_author_social_media_link($platform) {
 }
 
 function register_social_media_shortcodes() {
-    $platforms = array('linkedin', 'instagram', 'x', 'bluesky', 'mastodon', 'tiktok', 'facebook', 'tumblr', 'website', 'email');
+    $platforms = array('linkedin', 'portfolio', 'shop', 'instagram', 'x', 'bluesky', 'mastodon', 'tiktok', 'facebook', 'tumblr', 'youtube', 'website', 'email');
     foreach ($platforms as $platform) {
         add_shortcode($platform . '_link', function() use ($platform) {
             return get_author_social_media_link($platform);
