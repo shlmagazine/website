292808e2870d0fb1bf829220cc790031d7a0c357
Yiu-On <14921674+yshyonli2@users.noreply.github.com>
2025-07-14 19:42:57 -0700

    Delete old ellipsis conversion code

--- plugins/shlm-functionality/shlm-functionality-formatting.php
+++ plugins/shlm-functionality/shlm-functionality-formatting.php
@@ -12,13 +12,3 @@ add_filter('the_content', 'cmos_ellipsis_everywhere', 12);
 add_filter('the_excerpt', 'cmos_ellipsis_everywhere', 12);
 add_filter('comment_text', 'cmos_ellipsis_everywhere', 12);
 add_filter('widget_text', 'cmos_ellipsis_everywhere', 12);
-
-// function spaced_ellipsis_wptexturize($replacements) {
-//     // Replace the default ellipsis (…) with spaced version
-//     $replacements['…'] = '&nbsp;.&nbsp;.&nbsp;.`';
-//     $replacements['...'] = '&nbsp;.&nbsp;.&nbsp;.`';
-//     $replacements[' . . .'] = '&nbsp;.&nbsp;.&nbsp;.`';
-
-//     return $replacements;
-// }
-// add_filter('wptexturize_custom_replacements', 'spaced_ellipsis_wptexturize');
