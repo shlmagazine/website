e82ffc06a0c87f7a0c775b020a8edf838db26b03
Yiu-On <14921674+yshyonli2@users.noreply.github.com>
2025-07-14 19:48:35 -0700

    Attempt to more robustly replace three-dot ellipsis variant with spaced variant

--- plugins/shlm-functionality/shlm-functionality-formatting.php
+++ plugins/shlm-functionality/shlm-functionality-formatting.php
@@ -12,3 +12,13 @@ add_filter('the_content', 'cmos_ellipsis_everywhere', 12);
 add_filter('the_excerpt', 'cmos_ellipsis_everywhere', 12);
 add_filter('comment_text', 'cmos_ellipsis_everywhere', 12);
 add_filter('widget_text', 'cmos_ellipsis_everywhere', 12);
+
+// Replace three-dot ellipsis variant
+function replace_all_ellipses($text) {
+    $text = str_replace('…', '&nbsp;.&nbsp;.&nbsp;.', $text);
+    return $text;
+}
+add_filter('the_content', 'replace_all_ellipses', 12);
+add_filter('the_excerpt', 'replace_all_ellipses', 12);
+add_filter('comment_text', 'replace_all_ellipses', 12);
+add_filter('widget_text', 'replace_all_ellipses', 12);
