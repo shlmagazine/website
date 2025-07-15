c7dfb8743a932c97c45feb2c72000b8bc7b25558
Yiu-On <14921674+yshyonli2@users.noreply.github.com>
2025-07-14 19:42:36 -0700

    Catch different ellipsis variants

--- plugins/shlm-functionality/shlm-functionality-formatting.php
+++ plugins/shlm-functionality/shlm-functionality-formatting.php
@@ -1,9 +1,13 @@
 <?php
 // Replace many ellipses variants with CMOS-compliant version
 function cmos_ellipsis_everywhere($text) {
-    // Replace the ellipsis character after wptexturize has done its thing
-    return str_replace('…', '&nbsp;.&nbsp;.&nbsp;.`', $text);
+    // Match:
+    // - "..." (not part of "....")
+    // - "…"
+    // - any variant with spaces (e.g., ". . .", " . . . ", etc.)
+    return preg_replace('/(?<!\.)\.{3}(?!\.)|…|\s?\.\s\.\s\.\s?/u', '&nbsp;.&nbsp;.&nbsp;.', $text);
 }
+
 add_filter('the_content', 'cmos_ellipsis_everywhere', 12);
 add_filter('the_excerpt', 'cmos_ellipsis_everywhere', 12);
 add_filter('comment_text', 'cmos_ellipsis_everywhere', 12);
