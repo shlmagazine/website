2a054593bbf05405ceccccb279b7a89cdf4f4b9e
Yiu-On <14921674+yshyonli2@users.noreply.github.com>
2025-07-14 19:40:51 -0700

    Rename to cmos_ellipsis_everywhere

--- plugins/shlm-functionality/shlm-functionality-formatting.php
+++ plugins/shlm-functionality/shlm-functionality-formatting.php
@@ -1,13 +1,13 @@
 <?php
 // Replace many ellipses variants with CMOS-compliant version
-function spaced_ellipsis_everywhere($text) {
+function cmos_ellipsis_everywhere($text) {
     // Replace the ellipsis character after wptexturize has done its thing
-    return str_replace('…', '.&nbsp;.&nbsp;.', $text);
+    return str_replace('…', '&nbsp;.&nbsp;.&nbsp;.`', $text);
 }
-add_filter('the_content', 'spaced_ellipsis_everywhere', 12);
-add_filter('the_excerpt', 'spaced_ellipsis_everywhere', 12);
-add_filter('comment_text', 'spaced_ellipsis_everywhere', 12);
-add_filter('widget_text', 'spaced_ellipsis_everywhere', 12);
+add_filter('the_content', 'cmos_ellipsis_everywhere', 12);
+add_filter('the_excerpt', 'cmos_ellipsis_everywhere', 12);
+add_filter('comment_text', 'cmos_ellipsis_everywhere', 12);
+add_filter('widget_text', 'cmos_ellipsis_everywhere', 12);
 
 // function spaced_ellipsis_wptexturize($replacements) {
 //     // Replace the default ellipsis (…) with spaced version
