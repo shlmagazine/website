1c7ed7082419d855aa686856bbd5eeeb872908b7
Yiu-On <14921674+yshyonli2@users.noreply.github.com>
2025-07-18 20:39:42 -0700

    Replace ellipses in titles as well

--- plugins/shlm-functionality/shlm-functionality-formatting.php
+++ plugins/shlm-functionality/shlm-functionality-formatting.php
@@ -30,6 +30,7 @@ function cmos_ellipses_everywhere($text) {
         $text
     );
 }
+add_filter('the_title', 'cmos_ellipses_everywhere', 12);
 add_filter('the_content', 'cmos_ellipses_everywhere', 12);
 
 // function replace_ellipsis_after_texturize($text) {
