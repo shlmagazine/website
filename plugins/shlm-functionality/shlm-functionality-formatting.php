cd52cefb69043a27d7b37b7cc6a0e2b29d4f2d85
Yiu-On <14921674+yshyonli2@users.noreply.github.com>
2025-07-18 20:25:29 -0700

    Add case for if an ellipsis begins a line

--- plugins/shlm-functionality/shlm-functionality-formatting.php
+++ plugins/shlm-functionality/shlm-functionality-formatting.php
@@ -1,17 +1,30 @@
 <?php
 // Replace many ellipses variants with CMOS-compliant version
-function cmos_ellipsis_everywhere($text) {
-    // Match:
-    // - "..." (not part of "....")
-    // - "…"
-    // - any variant with spaces (e.g., ". . .", " . . . ", etc.)
-    return preg_replace('/(?<!\.)\.{3}(?!\.)|…|\s?\.\s\.\s\.\s?/u', '&nbsp;.&nbsp;.&nbsp;.', $text);
+function cmos_ellipses_everywhere($text) {
+    return preg_replace_callback(
+        // Match "..." or "…" or spaced ". . ." variants
+        // Look for start-of-string, <br>, or \n as possible line starts
+        '/(?:(?<=^)|(?<=<br\s*\/?>)|(?<=\n))(\s*)(\.{3}|…|\.\s\.\s\.)(?!\.)|(\.{3}|…|\.\s\.\s\.)(?!\.)/iu',
+        function ($matches) {
+            $ellipsis = '.&nbsp;.&nbsp;.';
+            
+            if (!empty($matches[2])) {
+                // Line-start version (match[2] comes from lookbehind branch)
+                return $ellipsis;
+            } elseif (!empty($matches[3])) {
+                // Mid-line version
+                return '&nbsp;' . $ellipsis;
+            } else {
+                return $matches[0]; // shouldn't happen, fallback
+            }
+        },
+        $text
+    );
 }
-
-add_filter('the_content', 'cmos_ellipsis_everywhere', 12);
-add_filter('the_excerpt', 'cmos_ellipsis_everywhere', 12);
-add_filter('comment_text', 'cmos_ellipsis_everywhere', 12);
-add_filter('widget_text', 'cmos_ellipsis_everywhere', 12);
+add_filter('the_content', 'cmos_ellipses_everywhere', 20);
+add_filter('the_excerpt', 'cmos_ellipses_everywhere', 20);
+add_filter('comment_text', 'cmos_ellipses_everywhere', 20);
+add_filter('widget_text', 'cmos_ellipses_everywhere', 20);
 
 // function replace_ellipsis_after_texturize($text) {
 //     return str_replace('…', '&nbsp;.&nbsp;.&nbsp;.', $text);
