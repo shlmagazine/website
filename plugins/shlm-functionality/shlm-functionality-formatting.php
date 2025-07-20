71c45ab0c0b12add706ced501b0a2b0fd007c89a
Yiu-On <14921674+yshyonli2@users.noreply.github.com>
2025-07-20 16:01:18 -0700

    Attempt fix for not converting

--- plugins/shlm-functionality/shlm-functionality-formatting.php
+++ plugins/shlm-functionality/shlm-functionality-formatting.php
@@ -1,30 +1,37 @@
 <?php
 // Replace many ellipses variants with CMOS-compliant version
 function cmos_ellipses_everywhere($text) {
-    return preg_replace_callback(
-        // Match line-start context: start of string, \n, or <br>
-        // Then optional whitespace and optional quotes
-        // Then one of: ...., ..., …, or . . .
-        // Then optional closing quote and whitespace
-        // End of line or <br> ensures it's alone on line
-        '/(^|\n|<br\s*\/?>)?\s*(["“”‘’\']?)\s*(\.{4}|\.{3}|…\s?|…|\.\s\.\s\.)\s*(["“”‘’\']?)\s*(?=$|\n|<br\s*\/?>)/',
-        function ($matches) {
-            $base_ellipsis = '.&nbsp;.&nbsp;.';
-            $trailing_dot = '';
-            $open_quote  = $matches[2] ?? '';
-            $raw_ellipsis = trim($matches[3] ?? '');
-            $close_quote = $matches[4] ?? '';
+    $base_ellipsis = '.&nbsp;.&nbsp;.';
 
-            // Handle "...."
-            if ($raw_ellipsis === '....') {
+    // Pass 1: Ellipses alone on line, possibly wrapped in quotes
+    $text = preg_replace_callback(
+        '/(^|\n|<br\s*\/?>)?\s*(["“”‘’\']?)\s*(\.{4}|\.{3}|…|\.\s\.\s\.)\s*(["“”‘’\']?)\s*(?=$|\n|<br\s*\/?>)/',
+        function ($matches) use ($base_ellipsis) {
+            $trailing_dot = '';
+            $ellipsis = trim($matches[3]);
+
+            if ($ellipsis === '....') {
                 $trailing_dot = '.';
             }
 
-            // Case: ellipsis is the only thing on the line (possibly wrapped in quotes)
+            $open_quote = $matches[2] ?? '';
+            $close_quote = $matches[4] ?? '';
+
             return $open_quote . $base_ellipsis . $trailing_dot . $close_quote;
         },
         $text
     );
+
+    // Pass 2: Normal inline ellipses (NOT alone on line)
+    $text = preg_replace_callback(
+        '/(?<!\.)\.{3}(?!\.)|…|\.\s\.\s\./',
+        function () use ($base_ellipsis) {
+            return '&nbsp;' . $base_ellipsis;
+        },
+        $text
+    );
+
+    return $text;
 }
 add_filter('the_title', 'cmos_ellipses_everywhere', 12);
 add_filter('the_content', 'cmos_ellipses_everywhere', 12);
