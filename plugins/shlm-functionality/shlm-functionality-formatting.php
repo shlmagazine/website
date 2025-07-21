0db91ab43517d31ecebb3a0d0790457a408a1063
Yiu-On <14921674+yshyonli2@users.noreply.github.com>
2025-07-20 18:32:51 -0700

    Revert to not rearranged version

--- plugins/shlm-functionality/shlm-functionality-formatting.php
+++ plugins/shlm-functionality/shlm-functionality-formatting.php
@@ -2,23 +2,26 @@
 // Replace ellipsis characters with CMOS-compliant version
 function cmos_ellipses_everywhere($text) {
     return preg_replace_callback(
-        '/(^“ *… *”$)              # Group 1: full quoted line
-        | (^ *… *$)                # Group 2: ellipsis alone on line
-        | (^ *… *(.))              # Group 3, 4: starts line
-        | (^\W *… *(.))            # Group 5, 6: quote + starts line
-        | ( *… *([^a-zA-Z0-9”\s])) # Group 7, 8: ellipsis + punctuation
-        | ( *…)                    # Group 9: all other ellipsis
-        /mx',
+        // Group 1: Ellipsis is followed by a punctuation mark (except a closing quote mark)
+        // Group 2: Group 1's punctuation mark
+        // Group 3: Ellipsis is the only character on a line, except quote marks
+        // Group 4: Ellipsis is the only character on a line
+        // Group 5: Ellipsis begins a line
+        // Group 6: Group 5's following character
+        // Group 7: An opening quote mark and an ellipsis begin a line
+        // Group 8: Group 7's following character
+        // Group 9: All other ellipsis cases
+        '/( *… *([^a-zA-Z0-9”\s]))|(^“ *… *”$)|(^ *… *$)|(^ *… *(.))|(^\W *… *(.))|( *…)/m',
         function ($matches) {
             $base_ellipsis = '.&nbsp;.&nbsp;.';
             $nbsp = '&nbsp;';
 
             return match (true) {
-                isset($matches[1]) => '“' . $base_ellipsis . '”' . ' g1',
-                isset($matches[2]) => $base_ellipsis . ' g2',
-                isset($matches[3]) => $base_ellipsis . $nbsp . $matches[4] . ' g3',
-                isset($matches[5]) => '“' . $base_ellipsis . $nbsp . $matches[6] . ' g4',
-                isset($matches[7]) => $nbsp . $base_ellipsis . $nbsp . $matches[8] . ' g7',
+                isset($matches[1]) => $nbsp . $base_ellipsis . $nbsp . $matches[2] . ' g1',
+                isset($matches[3]) => '“' . $base_ellipsis . '”' . ' g3',
+                isset($matches[4]) => $base_ellipsis . ' g4',
+                isset($matches[5]) => $base_ellipsis . $nbsp . $matches[6] . ' g5',
+                isset($matches[7]) => '“' . $base_ellipsis . $nbsp . $matches[8] . ' g7',
                 isset($matches[9]) => $base_ellipsis . ' g9',
                 default => $matches[0] . ' g0', // fallback
             };
