62ec6c831514e572f37bb18f5eb5b0ebcf832857
Yiu-On <14921674+yshyonli2@users.noreply.github.com>
2025-07-20 18:40:41 -0700

    Restrict replacement to just the brand page

--- plugins/shlm-functionality/shlm-functionality-formatting.php
+++ plugins/shlm-functionality/shlm-functionality-formatting.php
@@ -29,9 +29,12 @@ function cmos_ellipses_everywhere($text) {
         $text
     );
 }
-add_filter('the_title', 'cmos_ellipses_everywhere', 12);
-add_filter('the_content', 'cmos_ellipses_everywhere', 12);
-add_filter('pre_get_document_title', 'cmos_ellipses_everywhere', 20);
+function cmos_ellipses_conditionally() {
+    if (is_page('brand')) {
+        add_filter('the_content', 'cmos_ellipses_everywhere');
+    }
+}
+add_action('template_redirect', 'cmos_ellipses_conditionally');
 
 // function replace_ellipsis_after_texturize($text) {
 //     return str_replace('…', '&nbsp;.&nbsp;.&nbsp;.', $text);
