2ef23d0f89f85b7261215d5ca593e55ad646361c
Yiu-On <14921674+yshyonli2@users.noreply.github.com>
2025-07-20 19:48:52 -0700

    Update doc and debugging

--- plugins/shlm-functionality/shlm-functionality-formatting.php
+++ plugins/shlm-functionality/shlm-functionality-formatting.php
@@ -2,16 +2,16 @@
 // Replace ellipsis characters with CMOS-compliant version
 function cmos_ellipses_everywhere($text) {
     return preg_replace_callback(
-        // Group 1: Ellipsis is followed by a punctuation mark (except a closing quote mark)
-        // Group 2: Group 1's punctuation mark
-        // Group 3: Ellipsis is the only character on a line, except quote marks
-        // Group 4: Ellipsis is the only character on a line
-        // Group 5: Ellipsis begins a line
+        // Group 1: Ellipsis is the only character on a line, except quote marks
+        // Group 2: Ellipsis is the only character on a line
+        // Group 3: Ellipsis begins a line
+        // Group 4: Group 3's following character
+        // Group 5: An opening quote mark and an ellipsis begin a line
         // Group 6: Group 5's following character
-        // Group 7: An opening quote mark and an ellipsis begin a line
-        // Group 8: Group 7's following character
+        // Group 7: Ellipsis is followed by a punctuation mark
+        // Group 8: Group 7's punctuation mark
         // Group 9: All other ellipsis cases
-        '/(^“ *… *”$)|(^ *… *$)|(^ *… *(.))|(^\W *… *(.))|( *… *([.,:;?!]))|( *…)/m',
+        '/(^“ *… *”$)|(^ *… *$)|(^ *… *(.))|(^“ *… *(.))|( *… *([.,:;?!]))|( *…)/m',
         function ($matches) {
             return '<pre>' . print_r($matches, true) . '</pre>';
 
@@ -29,15 +29,15 @@ function cmos_ellipses_everywhere($text) {
             // };
             switch (true) {
                 case !empty($matches[1]):
-                    return '“' . $base_ellipsis . '”' . ' g3';
+                    return '“' . $base_ellipsis . '”' . ' g1';
                 case !empty($matches[2]):
-                    return $base_ellipsis . ' g4';
+                    return $base_ellipsis . ' g2';
                 case !empty($matches[3]):
-                    return $base_ellipsis . $nbsp . $matches[4] . ' g5';
+                    return $base_ellipsis . $nbsp . $matches[4] . ' g3';
                 case !empty($matches[5]):
-                    return '“' . $base_ellipsis . $nbsp . $matches[6] . ' g7';
+                    return '“' . $base_ellipsis . $nbsp . $matches[6] . ' g5';
                 case !empty($matches[7]):
-                    return $nbsp . $base_ellipsis . $nbsp . $matches[8] . ' g1';
+                    return $nbsp . $base_ellipsis . $nbsp . $matches[8] . ' g7';
                 case !empty($matches[9]):
                     return $base_ellipsis . ' g9';
                 default:
