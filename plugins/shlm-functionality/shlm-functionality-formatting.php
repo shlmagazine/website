a4446bc2f06277056cad8622607e72d64ff2494f
Yiu-On <14921674+yshyonli2@users.noreply.github.com>
2025-07-22 19:09:10 -0700

    Readd debugging

--- plugins/shlm-functionality/shlm-functionality-formatting.php
+++ plugins/shlm-functionality/shlm-functionality-formatting.php
@@ -45,12 +45,12 @@ function cmos_ellipses_everywhere($text) {
             $nbsp = '&nbsp;';
 
             return match (true) {
-                !empty($matches['alone_with_quotes']) => '“' . $base_ellipsis . '”',
-                !empty($matches['alone_on_line']) => $matches['alone_on_line_start_tag'] . $base_ellipsis . $matches['alone_on_line_end_tag'],
-                !empty($matches['start_of_line']) => $matches['start_of_line_start_tag'] . $base_ellipsis . $nbsp . $matches['start_of_line_character'],
-                !empty($matches['start_of_quote']) => $matches['start_of_quote_start_tag'] . '“' . $base_ellipsis . $nbsp . $matches['start_of_quote_character'],
-                !empty($matches['punctuation']) => $base_ellipsis . $nbsp . $matches['punctuation_mark'],
-                !empty($matches['general']) => $nbsp . $base_ellipsis . ' ',
+                !empty($matches['alone_with_quotes']) => '“' . $base_ellipsis . '”' . ' g1',
+                !empty($matches['alone_on_line']) => $matches['alone_on_line_start_tag'] . $base_ellipsis . $matches['alone_on_line_end_tag'] . ' g2',
+                !empty($matches['start_of_line']) => $matches['start_of_line_start_tag'] . $base_ellipsis . $nbsp . $matches['start_of_line_character'] . ' g3',
+                !empty($matches['start_of_quote']) => $matches['start_of_quote_start_tag'] . '“' . $base_ellipsis . $nbsp . $matches['start_of_quote_character'] . ' g5',
+                !empty($matches['punctuation']) => $base_ellipsis . $nbsp . $matches['punctuation_mark'] . ' g7',
+                !empty($matches['general']) => $nbsp . $base_ellipsis . ' ' . ' g9',
                 default => $matches[0] . ' g0', // fallback
             };
         },
