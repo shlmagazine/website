44f3c9c89d0c8bd9945c8e08d097e9364d8325df
Yiu-On <14921674+yshyonli2@users.noreply.github.com>
2025-07-24 19:22:35 -0700

    When an ellipsis begins a line, make the starting tag optional
    ]
    q
    :x

--- plugins/shlm-functionality/shlm-functionality-formatting.php
+++ plugins/shlm-functionality/shlm-functionality-formatting.php
@@ -16,7 +16,7 @@ function cmos_ellipses_everywhere($text) {
             
             # Ellipsis begins a line
             |(?<start_of_line>
-                ^(?<start_of_line_start_tag><.*>)
+                ^(?<start_of_line_start_tag><.*>)*
                 \ *…\ *(?<start_of_line_character>.)
             )
             
