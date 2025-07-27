54498e03fc16fce91db96fdd9f289ecc17e5befe
Yiu-On <14921674+yshyonli2@users.noreply.github.com>
2025-07-26 17:10:11 -0700

    Reenable

--- plugins/shlm-functionality/shlm-functionality-formatting.php
+++ plugins/shlm-functionality/shlm-functionality-formatting.php
@@ -69,9 +69,9 @@ function cmos_ellipses_everywhere($text) {
         $text
     );
 }
-// add_filter('the_title', 'cmos_ellipses_everywhere', 12);
-// add_filter('the_content', 'cmos_ellipses_everywhere', 12);
-// add_filter('pre_get_document_title', 'cmos_ellipses_everywhere', 20);
+add_filter('the_title', 'cmos_ellipses_everywhere', 12);
+add_filter('the_content', 'cmos_ellipses_everywhere', 12);
+add_filter('pre_get_document_title', 'cmos_ellipses_everywhere', 20);
 
 // function replace_ellipsis_after_texturize($text) {
 //     return str_replace('…', '&nbsp;.&nbsp;.&nbsp;.', $text);
