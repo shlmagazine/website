090c899322a79845bf444c63503e8c577240425b
Yiu-On <14921674+yshyonli2@users.noreply.github.com>
2025-07-31 12:11:31 -0700

    Disable default theme patterns

--- plugins/shlm-functionality/shlm-functionality-editor.php
+++ plugins/shlm-functionality/shlm-functionality-editor.php
@@ -12,8 +12,19 @@ function restrict_heading_levels( $args, $block_type ) {
 }
 add_filter( 'register_block_type_args', 'restrict_heading_levels', 10, 2 );
 
-// Disable default pattern directory
+// Disable default core patterns
 function theme_support() {
     remove_theme_support( 'core-block-patterns' );
 }
 add_action( 'after_setup_theme', 'theme_support', 11 );
+
+// Disable default theme patterns
+function remove_parent_theme_patterns() {
+    $registry = WP_Block_Patterns_Registry::get_instance();
+    foreach ( $registry->get_all_registered() as $slug => $pattern ) {
+        if ( str_starts_with( $slug, 'twentytwentyfour/' ) ) {
+            unregister_block_pattern( $slug );
+        }
+    }
+}
+add_action( 'init', 'remove_parent_theme_patterns', 20 );
