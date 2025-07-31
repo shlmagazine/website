00e66dd14446b94b280f93326cbc3cdd8c697a4c
Yiu-On <14921674+yshyonli2@users.noreply.github.com>
2025-07-31 13:11:53 -0700

    Attempt to disable certain formatting options in editor

--- plugins/shlm-functionality/disable-all-block-styles.js
+++ plugins/shlm-functionality/disable-all-block-styles.js
@@ -1,3 +1,11 @@
 // wp.domReady( () => {
 //     wp.blocks.unregisterBlockStyle( 'core/image', 'rounded' );
 // });
+
+wp.domReady( () => {
+    wp.richText.unregisterFormatType( 'core/image' );
+    wp.richText.unregisterFormatType( 'core/language' );
+    wp.richText.unregisterFormatType( 'core/keyboard' );
+    wp.richText.unregisterFormatType( 'core/subscript' );
+    wp.richText.unregisterFormatType( 'core/superscript' );
+});
