0ddd688a914dc4a5277dc2aa893eff64db9ff827
Yiu-On <14921674+yshyonli2@users.noreply.github.com>
2025-11-28 02:48:58 -0800

    Allowlist feedback page for Contact Form 7

--- plugins/shlm-functionality/shlm-functionality-cf7.php
+++ plugins/shlm-functionality/shlm-functionality-cf7.php
@@ -50,6 +50,20 @@ function load_wpcf7_scripts() {
       wpcf7cf_enqueue_styles();
     }
   }
+  if ( is_page('feedback') ) {
+    if ( function_exists( 'wpcf7_enqueue_scripts' ) ) {
+      wpcf7_enqueue_scripts();
+    }
+    if ( function_exists( 'wpcf7_enqueue_styles' ) ) {
+      wpcf7_enqueue_styles();
+    }
+    if ( function_exists( 'wpcf7cf_enqueue_scripts' ) ) {
+      wpcf7cf_enqueue_scripts();
+    }
+    if ( function_exists( 'wpcf7cf_enqueue_styles' ) ) {
+      wpcf7cf_enqueue_styles();
+    }
+  }
 }
 
 // Remap Contact Form 7 user roles so that they show up in the User Role Editor plugin
